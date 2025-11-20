897abcec1186656ee69430ae577c2a27ae6a56fb
koreoxy <rayssankn@gmail.com>
2025-11-20 16:25:50 +0700

    deploy vercel 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
+  // Load env file
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
@@ -14,20 +16,16 @@ export default defineConfig(({ mode }) => {
         projects: ['./tsconfig.json'],
       }),
       tailwindcss(),
+      tanstackStart(),
       viteReact(),
     ],
-
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: env.VITE_API_URL, // ← pakai ENV
           changeOrigin: true,
         },
       },
-    },
-
-    build: {
-      outDir: 'dist',
     },
   }
 })
